9504eafba56a62fae0b123c86d6041e077b5f2cb
Niclas Behrendt <niclas.behrendt@tallence.com>
2023-04-06 16:44:04 +0200

    init basic test

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
